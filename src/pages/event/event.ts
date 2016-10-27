d22324114801543ed3a318621b92e64561354675
Tristan Teague <tteague@atu.edu>
2016-10-27 17:59:09 -0500

    Add files via upload

---
+++ src/pages/event/event.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-event',
+  templateUrl: 'event.html'
+})
+export class Event {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
